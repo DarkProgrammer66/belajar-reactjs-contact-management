993d45438483f4b966674e8f300cfb7d7115b320
aliyudhati <aliyudhati12@gmail.com>
2025-11-09 21:09:47 +0700

    register, login and dashboard

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout.jsx";
+import UserRegister from "./components/User/UserRegister.jsx";
+import UserLogin from "./components/User/UserLogin.jsx";
+import DashboardLayout from "./components/DashboardLayout.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/login" element={<UserLogin />} />
+        </Route>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="contacts" element={<div>Contacts</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
